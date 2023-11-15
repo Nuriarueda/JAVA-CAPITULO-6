4774f6fef06212f4a1def56f046d6463805037e4
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-15 10:57:15 +0100

    capitulo 6

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -6,6 +6,72 @@ numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A (que sería el
 String.valueOf(n). */
 public class Ejercicio2 {
     public static void main(String[] args) {
+        String palo = " ";
+        String carta = " ";
+
+
+        int numCarta = (int)(Math.random()*13) + 1; 
+
+        switch(numCarta) {
+            case 1:
+                System.out.print("A");
+                break;
+            case 2:
+                System.out.print("2");
+            break;
+            case 3:
+                System.out.print("3"); 
+                break;
+            case 4:
+                System.out.print("4");
+                break;
+            case 5:
+                System.out.print("5");
+                break;
+            case 6:
+                System.out.print("6");
+                break;
+            case 7:
+                System.out.print("7");
+                break;
+            case 8:
+                System.out.print("8");
+                break;
+            case 9:
+                System.out.print("8");
+                break;
+            case 10:
+                System.out.print("10");
+                break;
+            case 11:
+                System.out.print("J");
+                break;
+            case 12:
+                System.out.print("Q");
+                break;
+            case 13:
+                System.out.print("K");
+                break;
+        }  
+
+        System.out.print(" de ");
+
+        int numPalo = (int)(Math.random()*4) + 1; 
+
+        switch (numPalo) {
+            case 1:
+                System.out.print("picas");
+                break;
+            case 2:
+                System.out.print("corazones");
+                break;
+            case 3:
+                System.out.print("diamantes");
+                break;
+            case 4:
+                System.out.print("tréboles");
+                break;
+        }
         
     }
 }
