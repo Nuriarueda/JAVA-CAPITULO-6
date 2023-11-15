8612c6d4717aa9a3026932fc1a19b8344b346018
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-15 10:43:12 +0100

    capitulo 6

---
+++ Ejercicio5.java
@@ -0,0 +1,8 @@
+/* Muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos)
+separados por espacios. Muestra también el máximo, el mínimo y la media
+de esos números. */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+    }
+}
