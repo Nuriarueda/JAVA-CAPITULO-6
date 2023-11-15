cbe3a398c54eac97ac63eb014e4686fa57853cc0
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-15 10:42:34 +0100

    capitulo 6

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,3 +1,6 @@
+/* Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separados por espacios. */
 public class Ejercicio4 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
