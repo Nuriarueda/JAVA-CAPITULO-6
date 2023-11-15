dedbeced3cdd4ebe827d5a9fb026831dfa054c64
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-15 10:39:14 +0100

    capitulo 6

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+/* Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+también la suma total (los puntos que suman entre los tres dados). */
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        int tirada;
+        int suma = 0;
+        System.out.print("Tirada de 3 dados: ");
+        for (int i = 0; i < 3; i++){
+            tirada = ((int)(Math.random()*6) + 1);
+            System.out.print(tirada + " ");
+            suma += tirada;
+        }
+
+        System.out.println("Suma: " + suma);
+
+    }
+}
