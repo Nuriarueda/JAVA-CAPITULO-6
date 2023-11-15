00e9f212c91c77d9880be07c27adca14c31716f6
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-15 10:42:06 +0100

    capitulo 6

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,3 +1,7 @@
+/* Igual que el ejercicio anterior pero con la baraja española. Se utilizará la baraja
+de 40 cartas: 2, 3, 4, 5, 6, 7, sota, caballo, rey y as. */
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
