467606243d3eacdcfb0233a653bef6f8bf8dd41b
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-16 12:14:29 +0100

    capitulo 6

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -1,3 +1,6 @@
+/* Realiza un programa que llene la pantalla de caracteres aleatorios (a lo Matrix)
+con el código ascii entre el 32 y el 126. Puedes hacer casting con (char) para
+convertir un entero en un carácter. */
 public class Ejercicio12 {
     public static void main(String[] args) {
         
