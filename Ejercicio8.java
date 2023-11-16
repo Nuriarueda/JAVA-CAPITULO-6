2263f08862c0c86a2ebf31b7484902fc62a29e3d
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-16 12:13:03 +0100

    capitulo 6

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,3 +1,6 @@
+/* Modifica el programa anterior para que la probabilidad de que salga un 1 sea
+de 1/2, la probabilidad de que salga x sea de 1/3 y la probabilidad de que salga
+2 sea de 1/6. Pista: 1/2 = 3/6 y 1/3 = 2/6. */
 public class Ejercicio8 {
     public static void main(String[] args) {
         
--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,3 +1,8 @@
+/* Realiza un programa que vaya generando números aleatorios pares entre 0
+y 100 y que no termine de generar números hasta que no saque el 24. El
+programa deberá decir al final cuántos números se han generado. */
 public class Ejercicio9 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
