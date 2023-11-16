5b66cfa13cdeede004f877fc8d8714e2dfcd6920
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-16 12:08:46 +0100

    capitulo 6

---
+++ Ejercicio6.java
@@ -0,0 +1,12 @@
+/* Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento fallido,
+el programa dirá cuántas oportunidades quedan y si el número introducido es
+menor o mayor que el número secreto. */
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
