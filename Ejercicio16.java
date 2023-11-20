5ef0bf86053a7c1c80fb3fa632b62c9a98028697
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-20 09:09:20 +0100

    capitulo 6

--- Ejercicio16.java
+++ Ejercicio16.java
@@ -1,3 +1,13 @@
+/* Realiza un simulador de máquina tragaperras simplificada que cumpla los
+siguientes requisitos:
+a) El ordenador mostrará una tirada que consiste en mostrar 3 figuras. Hay 5
+figuras posibles: corazón, diamante, herradura, campana y limón.
+b) Si las tres figuras son diferentes se debe mostrar el mensaje “Lo siento, ha
+perdido”.
+c) Si hay dos figuras iguales y una diferente se debe mostrar el mensaje “Bien,
+ha recuperado su moneda”.
+d) Si las tres figuras son iguales se debe mostrar “Enhorabuena, ha ganado
+10 monedas”. */
 public class Ejercicio16 {
     public static void main(String[] args) {
         
