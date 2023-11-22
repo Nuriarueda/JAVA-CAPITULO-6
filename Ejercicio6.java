a293032326c1e7cce06542f52fbab9f1dafb0ee2
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-22 17:42:50 +0100

    capitulo 6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -7,6 +7,29 @@ import java.util.Scanner;
 public class Ejercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.println("Adivina un numero entre 0 y 100 tienes 5 oportunidades");
+        int numero= (int)(Math.random()*101);
+        boolean acierto=false;
+        int i=0;
+            do{
+                int intento=sc.nextInt();
+                if(intento==numero){
+                    acierto=true;
+                    System.out.print("Acertaste");
+                }else{
+                    if(intento<numero){
+                        System.out.println("Fallaste, has puesto un numero menor");
+                    }else{
+                        System.out.println("Fallaste, has puesto un numero mayor");
+                    }
+                }
+                i++;
+                if(i>=5){
+                    break;
+                }
+            }while(!acierto);
+        
         sc.close();
     }
 }
