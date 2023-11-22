53103c7aa0e15ac63455dc3087829cdd5c407fe9
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-22 17:46:11 +0100

    capitulo 6

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -4,6 +4,34 @@ de los siguientes: *, -, =, ., |, @. Las líneas deben tener una longitud aleato
 entre 1 y 40 caracteres. */
 public class Ejercicio10 {
     public static void main(String[] args) {
-        
+        System.out.println("Este programa te muestra 4 renglones de caracteres aleatorios");
+
+        for(int j=1;j<=4;j++){
+            for(int i=1;i<=40;i++){
+                int resultado= (int)(Math.random()*7+1);
+                switch(resultado){
+                    case 1:
+                    System.out.print("*");
+                    break;
+                    case 2:
+                    System.out.print("-");
+                    break;
+                    case 3:
+                    System.out.print("=");
+                    break;
+                    case 4:
+                    System.out.print(".");
+                    break;
+                    case 5:
+                    System.out.print("|");
+                    break;
+                    case 6:
+                    System.out.print("@");
+                    break;
+                }
+                
+            }
+            System.out.println("");
+        }
     }
 }
