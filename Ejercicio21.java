ee6d3df80ada0b9e03275e119fcf4f35f2841211
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-22 17:54:30 +0100

    capitulo 6

--- Ejercicio21.java
+++ Ejercicio21.java
@@ -1,3 +1,7 @@
+/* Realiza un programa que genere una secuencia de cinco monedas de curso
+legal lanzadas al aire. Las monedas disponibles son de 1 céntimo, 2 céntimos,
+5 céntimos, 10 céntimos, 20 céntimos, 50 céntimos, 1 euro y 2 euros. Las dos
+posiciones posibles son cara y cruz. */
 public class Ejercicio21 {
     public static void main(String[] args) {
         System.out.println("Este programa lanza monedas");
