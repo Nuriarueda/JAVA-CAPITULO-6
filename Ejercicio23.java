3014fa3c57ddda10a9da1e8b4bcfe469c570dca4
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-23 08:24:34 +0100

    capitulo 6

---
+++ Ejercicio23.java
@@ -0,0 +1,7 @@
+/* Las caras de un dado de poker tienen las siguientes figuras: As, K, Q, J, 7 y 8.
+Escribe un programa que genere de forma aleatoria la tirada de cinco dados. */
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        
+    }
+}
