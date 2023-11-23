ebebb0ef63ea5dbd962dfb9878a27fc52b73b088
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-23 08:23:53 +0100

    capitulo 6

---
+++ Ejercicio22.java
@@ -0,0 +1,13 @@
+/* Realiza un programa que pinte por pantalla una serpiente con un “serpenteo”
+aleatorio. La cabeza se representará con el carácter @ y se debe colocar
+exactamente en la posición 13 (con 12 espacios delante). A partir de ahí,
+el cuerpo irá serpenteando de la siguiente manera: se generará de forma
+aleatoria un valor entre tres posibles que hará que el siguiente carácter se
+coloque una posición a la izquierda del anterior, alineado con el anterior o una
+posición a la derecha del anterior. La longitud de la serpiente se pedirá por
+teclado y se supone que el usuario introducirá un dato correcto. */
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        
+    }
+}
