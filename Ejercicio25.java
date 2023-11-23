34c2cfa8156229229100d9dc7d1f5a90ab67d886
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-23 08:26:02 +0100

    capitulo 6

---
+++ Ejercicio25.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que muestre por pantalla 100 números enteros separados
+por un espacio. Los números deben estar generados de forma aleatoria en
+un rango entre 10 y 200 ambos incluidos. Los primos deben aparecer entre
+almohadillas (p. ej. #19#) y los múltiplos de 5 entre corchetes (p. ej. [25]). */
+public class Ejercicio25 {
+    public static void main(String[] args) {
+        
+    }
+}
