fb29e8d88a30578ea26ecc031a9d4daf626b05ef
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-23 08:25:23 +0100

    capitulo 6

---
+++ Ejercicio24.java
@@ -0,0 +1,7 @@
+/* Escribe un programa que, dado un número introducido por teclado, elija al azar
+uno de sus dígitos. */
+public class Ejercicio24 {
+    public static void main(String[] args) {
+        
+    }
+}
