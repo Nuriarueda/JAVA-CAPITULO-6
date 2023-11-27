9a17d5cc7ce73d558acd6747e799dc303b96d6b2
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-27 08:38:24 +0100

    capitulo 6

--- Ejercicio29.java
+++ Ejercicio29.java
@@ -1,3 +1,9 @@
+/* Realiza un programa que muestre la previsión del tiempo para mañana en
+Málaga. Las temperaturas máxima y mínima se deben generar de forma
+aleatoria entre los intervalos máximos y mínimos absolutos medidos en las
+últimas décadas para cada estación. La probabilidad de que esté soleado
+o nublado en cada estación se proporciona a continuación. Obviamente, la
+temperatura mínima deberá ser menor o igual que la temperatura máxima. */
 public class Ejercicio29 {
     public static void main(String[] args) {
         
