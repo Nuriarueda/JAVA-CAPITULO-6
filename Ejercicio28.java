0478c79d7e588fd8d2c08e7172133c24f5e9a9f6
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-27 08:38:04 +0100

    capitulo 6

--- Ejercicio28.java
+++ Ejercicio28.java
@@ -1,4 +1,12 @@
-/*  */
+/* Realiza un programa que sea capaz de recolocar los números de un array de
+fuera hacia adentro. En primer lugar, el programa pedirá al usuario el tamaño
+del array. A continuación generará un array con ese tamaño con números
+enteros aleatorios entre 0 y 200 ambos incluidos. Seguidamente el programa
+irá colocando desde fuera hacia adentro los números de tal forma que el
+primero se coloca en la primera posición, el segundo en la última, el tercero
+en la segunda, el cuarto en la penúltima, el quinto en la tercera, en sexto en la
+antepenúltima, etc. Se debe mostrar por pantalla tanto el array original como
+el array resultado. */
 public class Ejercicio28 {
     public static void main(String[] args) {
         
