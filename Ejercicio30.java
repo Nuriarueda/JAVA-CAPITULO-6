6c2657c25f207078b857e432c34678804df285ff
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-27 08:38:47 +0100

    capitulo 6

--- Ejercicio30.java
+++ Ejercicio30.java
@@ -1,3 +1,10 @@
+/* El pequeño Roberto tenía como mascota un pececillo dentro de una pecera. Los
+Reyes Magos le han traído un caballito de mar ($) y una caracola (@) para que
+le hagan compañía al pez. Realiza un programa que pinte por pantalla la pecera
+con los tres animalitos acuáticos colocados dentro en posiciones aleatorias. Por
+una cuestión de física elemental, ninguno de los animales puede coincidir
+en la misma posición. Se debe pedir al usuario el ancho y el alto de la pecera,
+que como mínimo serán de 4 unidades. */
 public class Ejercicio30 {
     public static void main(String[] args) {
         
