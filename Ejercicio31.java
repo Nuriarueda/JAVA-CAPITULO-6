124d5a4e632439251bb8dbe3f451045e56de201c
Nuriarueda <ruedareinanuria@gmail.com>
2023-11-28 19:14:20 +0100

    capitulo 6

--- Ejercicio31.java
+++ Ejercicio31.java
@@ -8,8 +8,54 @@ da ninguno de los casos anteriores, es decir si sale 4, 5, 6, 8, 9 o 10, el jueg
 entra en una segunda etapa. En esta etapa, el jugador buscará volver a obtener
 ese número en los dados. Si consigue repetir ese número, gana. Si sale un 7,
 pierde. Si sale otro número, tiene que seguir tirando. */
+
+import java.util.Scanner;
+
 public class Ejercicio31 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Con este programa jugaremos al Craps ");
+        System.out.println("¿Cuanto dinero quieres apostar?");
+        int dinero=sc.nextInt();
+        System.out.print("La tirada de los datos es:");
+        int dado1=0;
+        int dado2=0;
+        int suma;
+        boolean seguir=true;
+        dado1=(int)(Math.random()*6+1);
+        dado2=(int)(Math.random()*6+1);
+        suma=dado1+dado2;
+        System.out.println(suma);
+        int suma2=0;
+        if(suma==7 || suma==11){
+            System.out.print("Has duplicado tu apuesta, has conseguido: "+dinero*2);
+        }else{
+            if(suma==2 || suma==3 || suma==12){
+                System.out.print("Has perdido todo el dinero");
+            }else{
+                System.out.println("Volvemos a tirar");
+            
+                while(seguir){
+                    dado1=(int)(Math.random()*6+1);
+                    dado2=(int)(Math.random()*6+1);
+                    suma2=dado1+dado2;
+                    if(suma2==suma){
+                        System.out.println(suma2+" has ganado");
+                        seguir=false;
+                    }else{
+                        if(suma2==7){
+                            System.out.println(suma2+" has perdido todo");
+                            seguir=false;
+                        }else{
+                            System.out.println(suma2);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        sc.close();
     }
 }
